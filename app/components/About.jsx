9fae7c954652448f0168278476e3b7198f7ea894
VL Consultants <peter.smink.nl@gmail.com>
2016-05-09 21:28:28 +0200

    Added copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,5 @@
 var React = require('react');
 
-var About = (props) => {
-  return (
-    <h3>About Component</h3>
-  );
-};
+var About = (props) => <h3>About Component</h3>;
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
 
 var Examples = (props) => {
-  return (
+  <div>
     <h3>Examples Component!</h3>
-  );
+    <p>Welcome to the examples page!</p>
+  </div>
 };
 
 module.exports = Examples;
